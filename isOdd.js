db0ffe6417f2f0d3258aec657f6e4598023f35ff
byungju0624 <byungju0624@gmail.com>
2021-04-27 09:22:30 +0900

    isOdd

---
+++ isOdd.js
@@ -0,0 +1,9 @@
+function isOdd(num) {
+  if (num === 0) return false;
+  else if (num === 1) return true;
+
+  if (num < 0) return isOdd(-num);
+  return isOdd(num - 2);
+}
+
+console.log(isOdd(451));
