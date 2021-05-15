43c7ecf8f4c89b7bed0dcac63b674985a5b3602c
정병주 <jeongbyeongju@jeongbyeongjuui-MacBookPro.local>
2021-05-15 20:32:53 +0900

    flattenArr

---
+++ flattenArr.js
@@ -0,0 +1,17 @@
+function flattenArr(arr) {
+    if(arr.length === 0) return [];
+   
+    const head = arr[0];
+    const tail = arr.slice(1);
+    if(Array.isArray(head)){ 
+      return flattenArr([...head,...tail]);
+      }
+    else{
+      return [head].concat(flattenArr(tail));
+    }
+   }
+   let output = flattenArr([[1], 2, [3, 4], 5]);
+   console.log(output); // --> [1, 2, 3, 4, 5]
+   
+  output = flattenArr([[2, [[3]]], 4, [[[5]]]])
+   console.log(output); // --> [2, 3, 4, 5]
