7348b1b06abf3fe422784f9350c89bf3d0fe8fdc
byungju0624 <byungju0624@gmail.com>
2021-07-26 06:17:21 +0900

    two pointer Algorithm study in arrayMerge solution

--- Algorithm/biggerNumber.js
+++ Algorithm/biggerNumber.js
@@ -1,6 +1,6 @@
 function solution(n, k, card) {
   let answer;
-  let tmp = new Set();
+  let tmp = new Set(); //중복제거하기위해 사용
   for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
       for (let k = j + 1; k < n; k++) {
